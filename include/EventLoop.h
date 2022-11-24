e22bf8078c2d11888e95a2f6a8dfcb557836718d
RrrrCrimson <995076985@qq.com>
2022-11-24 16:27:09 +0800

    EvtHandle : Poll process controlled by EventLoop.

--- include/EventLoop.h
+++ include/EventLoop.h
@@ -6,6 +6,8 @@
 #define EVTHANDLE_EVENTLOOP_H
 #include "noncopyable.h"
 #include <pthread.h>
+#include <vector>
+#include <memory>
 
 namespace EvtHandle {
 class Channel;
@@ -15,6 +17,7 @@ public :
     EventLoop();
     ~EventLoop();
     void loop();
+    void quit();
     void UpdateChannel(Channel* channel);
 //    bool IsInLoopThread() const {
 //        return threadId_ == pthread_self();
@@ -26,8 +29,12 @@ public :
 //    }
 private:
     void abortNotInLoopThread();
-    bool isLooping_;
-    const pid_t threadId_;
+    using ChannelList = std::vector<Channel*>;
+    bool isLooping_{};
+    bool quit_{};
+    const pid_t threadId_{};
+    std::unique_ptr<Poller> poller_{};
+    ChannelList activeChannels_{};
 };
 }
 #endif //EVTHANDLE_EVENTLOOP_H
--- src/EventLoop.cpp
+++ src/EventLoop.cpp
@@ -2,9 +2,12 @@
 // Created by rrrr on 22-11-22.
 //
 #include "EventLoop.h"
+#include "Poller.h"
+#include "Channel.h"
 #include <iostream>
 namespace EvtHandle {
 __thread EventLoop* t_loopInThisThread = 0;
+const int kPollTimeMs = 10000;
 
 
 EventLoop::EventLoop() :isLooping_(false), threadId_(pthread_self()) {
@@ -22,10 +25,25 @@ EventLoop::~EventLoop() {
 }
 
 void EventLoop::loop() {
+    if(isLooping_) {return;}
     isLooping_ = true;
     ///todo : poll
-
+    while(!quit_) {
+        activeChannels_.clear();
+        poller_->Poll(10000, &activeChannels_);
+        for(auto & activeChannel : activeChannels_) {
+            activeChannel->handleEvent();
+        }
+    }
     std::cout<< "EventLoop " << this << " start looping" << std::endl;
     isLooping_ = false;
+}
+
+void EventLoop::UpdateChannel(Channel *channel) {
+    poller_->UpdateChannel(channel);
+}
+
+void EventLoop::quit() {
+    quit_ = true;
 }
 }
